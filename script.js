cdfb5745fc1a0b03a31752c3c198d65b57565581
danieljkim1127 <danieljkim1127@gmail.com>
2025-06-12 19:00:54 -0400

    updates after yd game

--- script.js
+++ script.js
@@ -9,103 +9,103 @@ const players = {
   ],
   valorant: [
     {
-      name: "JW",
+      name: `JW`,
       image: "images/Jw.jpg",
       score: 1,
       description: "Usually frags a lot, great aim, consistent, solid game sense. Great first pick to fill out duelist role. No dick riding."
     },
     {
-      name: "Dylan",
+      name: `Ethan`,
+      image: "images/ethan.jpg",
+      score: 1.8,
+      description: "Good aim, great game sense, great flex player and can lead shittier teammates. Recently came back to form."
+    },
+    {
+      name: `Dylan`,
       image: "images/dylan.PNG",
-      score: 2,
+      score: 2.1,
       description: "Very good aim and very consistent. Great pick for duelist that can actually frag."
     },
     {
-      name: "Ethan",
-      image: "images/ethan.jpg",
-      score: 2.2,
-      description: "Good aim, great game sense, great flex player and can lead shittier teammates, but recently has not been fragging up to expectations."
-    },
-    {
-      name: "Hwang",
+      name: `Hwang`,
       image: "images/hwang.JPG",
       score: 3,
-      description: "Great aim, good game sense, solid duelist. Either stomps or does okay."
+      description: "Great aim, good game sense, solid duelist. Either stomps or does okay. Needs to grow taller."
     },
     {
-      name: "Michael",
+      name: `Michael`,
       image: "images/mike.jpeg",
       score: 3.3,
       description: "Great aim and good game sense, but has not played in awhile. Limited agent pool."
     },
     {
-      name: "Eugene",
+      name: `Eugene`,
       image: "images/eugene.JPG",
       score: 4,
-      description: "Good aim. Walmart Dylan."
+      description: "Good aim. Walmart Dylan. Needs to be arrested."
     },
     {
-      name: "Matt",
+      name: `Matt`,
       image: "images/matt.JPG",
       score: 4.2,
       description: "Solid aim, great game sense, and a playmaker, but needs good support to execute. Solid flex player."
     },
     {
-      name: "Jared",
+      name: `Jared`,
       image: "images/jared.JPG",
       score: 4.7,
       description: "Good player, but definitely barks more than he bites. Lowkey zesty."
     },
     {
-      name: "Brian Ju",
+      name: `Brian Ju`,
       image: "images/brianju.PNG",
       score: 5.1,
       description: "All aim no brain. Fills initiator role."
     },
     {
-      name: "Schatz",
+      name: `Schatz "Mr Unclutchable"`,
       image: "images/schatz.PNG",
       score: 6.2,
       description: "Great sova player. Has recently been doing very well, but probably overvalued at the moment."
     },
     {
-      name: "Noah",
+      name: `Noah`,
       image: "images/noah.PNG",
       score: 7.6,
       description: "A true role player. Plays what the team needs and has great communication. A misfortune that his aim is ass."
     },
     {
-      name: "JBL",
+      name: `JBL`,
       image: "images/boomer.jpeg",
       score: 7.8,
       description: "Plays off meta picks and strats. Lowkey troll."
     },
     {
-      name: "Ron",
+      name: `Ron`,
       image: "images/ron.JPG",
       score: 8.6,
       description: "Used to be good. Used to."
     },
     {
-      name: "Jacob",
+      name: `Jacob`,
       image: "images/jacob.JPG",
       score: 8.8,
       description: "I aint never seen this guy play but everyone talks like he's ass."
     },
     {
-      name: "Dank",
+      name: `Dank`,
       image: "images/dank.jpeg",
       score: 9.5,
       description: "Still waiting for viper buffs. Only plays when 10 man needs to be filled."
     },
     {
-      name: "JL",
+      name: `JL`,
       image: "images/jl.JPG",
       score: 9.8,
       description: "Recently been given wins as charity due to his brutal loss streak. This guy is straight ass."
     },
     {
-      name: "Josh",
+      name: `Josh`,
       image: "images/josh.JPG",
       score: null,
       description: "I haven't seen this guy play yet but wanted to include him in this list so he doesn't feel left out. Lemme supp you alistar now."
