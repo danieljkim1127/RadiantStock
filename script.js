c4bd5fd6a18a6cffc612d45a3383c7e826f6898f
danieljkim1127 <danieljkim1127@gmail.com>
2025-06-13 13:16:04 -0400

    league ranks

--- script.js
+++ script.js
@@ -1,16 +1,115 @@
 const players = {
   league: [
     {
-      name: "Coming Soon",
-      image: "https://example.com/faker.jpg",
-      score: 5318008,
-      description: "Coming Soon"
-    }
+      name: `Hwang`,
+      image: "images/hwang.JPG",
+      score: 1,
+      change: 0,
+      description: "Thanos Incarnate. Everyone has been traumatized by this man at some point. Always ban vayne. Needs to grow taller."
+    },
+    {
+      name: `Josh - Dank's biggest hater`,
+      image: "images/josh.JPG",
+      score: 2.2,
+      change: 0,
+      description: "Consistent high performer, great macro. Enough with the dick riding, lemme supp you alistar now."
+    },
+    {
+      name: `Noah`,
+      image: "images/noah.PNG",
+      score: 2.4,
+      change: 0,
+      description: "Great macro, great leader, but does not carry the presence one would hope for in their carry."
+    },
+    {
+      name: `Croissant`,
+      image: "images/croissant.webp",
+      score: 2.6,
+      change: 0,
+      description: "Can fill many roles well and consistently wins."
+    },
+    {
+      name: `Dank`,
+      image: "images/dank.jpeg",
+      score: 3.4,
+      change: 0,
+      description: "Can fill many roles but has yet to go crazy. Negative outlook on dankstock due to his biggest hater. Currently on fraud watch."
+    },
+    {
+      name: `Gino "Ubuditcher" Kim`,
+      image: "images/gino.jpg",
+      score: 3.7,
+      change: 0,
+      description: "New challenger. Beat hwang, wrecked eugene. Top/Jungle main. GinoCoin to the moon?."
+    },
+    {
+      name: `JL`,
+      image: "images/jl.JPG",
+      score: 4.1,
+      change: 0,
+      description: "A Lee Sin jungle one trick. 'His Lee Sin is not that good' - Josh, Croissant. Consistently loses to Croissant's jungle. "
+    },
+    {
+      name: `Matt`,
+      image: "images/matt.JPG",
+      score: 4.7,
+      change: 0,
+      description: "Solid team player, mainly mid, complains his leblanc always gets banned."
+    },
+    {
+      name: `Jared`,
+      image: "images/jared.JPG",
+      score: 4.8,
+      change: 0,
+      description: "Thinks hes the main character. Ur not that guy pal."
+    },
+    {
+      name: `JBL`,
+      image: "images/boomer.jpeg",
+      score: 5.1,
+      change: 0,
+      description: "Plays off meta picks and strats, but he makes it work. Undeniable pain in the ass."
+    },
+    {
+      name: `Eugene`,
+      image: "images/eugene.JPG",
+      score: 6.2,
+      change: 0,
+      description: "NAs best trundle. Stop jerkin."
+    },
+    {
+      name: `Junsoo "Gubi Goobers" Lee`,
+      image: "images/junsoo.jpeg",
+      score: 6.7,
+      change: 0,
+      description: "Infamous for his baron smite and blaming the sejuani character model. Negatively viewed by many, but highly undervalued."
+    },
+    {
+      name: `Schatz`,
+      image: "images/schatz.PNG",
+      score: 7.1,
+      change: 0,
+      description: "So far only seen in the support role. He's a good boy."
+    },
+    {
+      name: `Ethan`,
+      image: "images/ethan.jpg",
+      score: 8.2,
+      change: 0,
+      description: "New to the game, support main. Stick to support."
+    },
+    {
+      name: `Ron`,
+      image: "images/ron.JPG",
+      score: 8.4,
+      change: 0,
+      description: "Ban Yuumi and Pyke. Tf he gunna do?"
+    },
   ],
   valorant: [
     {
       name: `JW`,
-      image: "images/Jw.jpg",
+      image: "images/jw.PNG",
       score: 1.5,
       change: -.5,
       description: "Usually frags a lot, great aim, consistent, solid game sense. Great first pick to fill out duelist role. Has not played in a minute due to studying."
@@ -23,32 +122,25 @@ const players = {
       description: "Good aim, great game sense, great flex player and can lead shittier teammates. Recently came back to form."
     },
     {
-      name: `Dylan`,
-      image: "images/dylan.PNG",
-      score: 2.1,
-      change: 0,
-      description: "Very good aim and very consistent. Great pick for duelist that can actually frag."
-    },
-    {
       name: `Hwang`,
       image: "images/hwang.JPG",
-      score: 3,
-      change: 0,
+      score: 2,
+      change: 1,
       description: "Great aim, good game sense, solid duelist. Either stomps or does okay. Needs to grow taller."
     },
     {
       name: `Michael`,
       image: "images/mike.jpeg",
-      score: 3.3,
-      change: 0,
+      score: 2.3,
+      change: 1,
       description: "Great aim and good game sense, but has not played in awhile. Limited agent pool."
     },
     {
-      name: `Eugene`,
-      image: "images/eugene.JPG",
-      score: 4,
-      change: 0,
-      description: "Good aim. Walmart Dylan. Needs to be arrested."
+      name: `Dylan`,
+      image: "images/dylan.PNG",
+      score: 3.1,
+      change: -1,
+      description: "Very good aim and very consistent. Great pick for duelist that can actually frag."
     },
     {
       name: `Matt`,
@@ -56,6 +148,13 @@ const players = {
       score: 4.2,
       change: 0,
       description: "Solid aim, great game sense, and a playmaker, but needs good support to execute. Solid flex player."
+    },
+    {
+      name: `Eugene`,
+      image: "images/eugene.JPG",
+      score: 4.4,
+      change: -.4,
+      description: "Good aim. Walmart Dylan. Needs to be arrested."
     },
     {
       name: `Jared`,
@@ -88,44 +187,44 @@ const players = {
     {
       name: `JBL`,
       image: "images/boomer.jpeg",
-      score: 7.8,
-      change: 0,
+      score: 8.1,
+      change: -0.3,
       description: "Plays off meta picks and strats. Lowkey troll."
-    },
-    {
-      name: `Ron`,
-      image: "images/ron.JPG",
-      score: 8.6,
-      change: 0,
-      description: "Used to be good. Used to."
     },
     {
       name: `Josh`,
       image: "images/josh.JPG",
-      score: 8.9,
-      change: 0,
+      score: 9.1,
+      change: -0.2,
       description: "I haven't seen this guy play yet but wanted to include him in this list so he doesn't feel left out. Lemme supp you alistar now."
+    },
+    {
+      name: `Ron`,
+      image: "images/ron.JPG",
+      score: 9.20,
+      change: -0.6,
+      description: "Used to be good. Used to."
     },
     {
       name: `Dank`,
       image: "images/dank.jpeg",
-      score: 9.2,
-      change: 0.3,
+      score: 9.5,
+      change: -0.3,
       description: "Still waiting for viper buffs. Only plays when 10 man needs to be filled."
-    },
-    {
-      name: `JL`,
-      image: "images/jl.JPG",
-      score: 9.6,
-      change: 0.1,
-      description: "Recently been given wins as charity due to his brutal loss streak. This guy is straight ass."
     },
     {
       name: `Jacob`,
       image: "images/jacob.JPG",
-      score: 9.8,
-      change: -1,
-      description: "Thank god he's at least tall. Thank god."
+      score: 9.6,
+      change: +0.2,
+      description: "He posted the black square twice. How good can he be?"
+    },
+    {
+      name: `JL`,
+      image: "images/jl.JPG",
+      score: 10,
+      change: -0.4,
+      description: "Recently been given wins as charity due to his brutal loss streak. This guy is straight ass."
     },
   ]
 };
